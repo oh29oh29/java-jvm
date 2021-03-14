53889aea5d3a486c3ddc299530194731af4be12a
oh29oh29 <oh29oh29.develop@gmail.com>
2021-03-14 21:02:43 +0900

    [JVM] javac javap java

--- src/main/java/CompileTest.java
+++ src/main/java/CompileTest.java
@@ -1,3 +1,9 @@
+/**
+ * 해당 클래스는 javac 를 사용하여 compile 하고
+ * compile 된 CompileTest.class 에 대하여 javap -c 명령어를 사용하여
+ * 바이트코드가 어떻게 작성되어 있는가를 확인하기 위해 작성되었다.
+ * */
+
 public class CompileTest {
 
     public static void main(String[] args) {
